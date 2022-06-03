b1bd5e599e32041373cba6d173e981000fad86d1
holms <anna61c9@edu.zealand.dk>
2022-06-03 09:59:18 +0200

    link til sider og artikel afsnit igang.

--- Kontakt.php
+++ Kontakt.php
@@ -27,6 +27,27 @@
 <!-- i <body> har man alt indhold på siden som brugeren kan se -->
 <body>
 
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item active" aria-current="page">Home</li>
+    </ol>
+</nav>
+
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Library</li>
+    </ol>
+</nav>
+
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item"><a href="#">Library</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Data</li>
+    </ol>
+</nav>
+
 <!-- Her skal sidens indhold ligge -->
 
 </body>
