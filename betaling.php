d450e0f9f121f411709bafbb547d9150b28a6113
spers <spersson@hotmail.com>
2022-06-15 00:35:43 +0200

    Lidt flere ændringer.

--- betaling.php
+++ betaling.php
@@ -21,11 +21,7 @@
 
         function activateButton(element) {
 
-            if (element.checked) {
-                document.getElementById("submit").disabled = false;
-            } else {
-                document.getElementById("submit").disabled = true;
-            }
+            document.getElementById("submit").disabled = !element.checked;
         }
     </script>
 
@@ -34,7 +30,7 @@
 <!-- i <body> har man alt indhold på siden som brugeren kan se -->
 <body>
 
-<section class="main">
+<section class="pt-5 pb-5">
     <div class="container">
         <div class="row">
             <div class="col-12">
@@ -42,89 +38,136 @@
             </div>
             <div class="col-sm-6 mb-3 text-md-left">
                 <a href="kurv.php">
-                    <i class=""><img src="images/Tegnebræt%208.png" alt="fjern" width="30"></i></a>
+                    <i class=""><img src="images/Tegnebræt%208.png" alt="tilbage" width="30"></i></a>
             </div>
         </div>
     </div>
+
+
+
     <form>
-        <div class="container mt-5">
+        <div class="container bg-light mt-5 py-3 rounded shadow-sm">
+            <div class="row">
+                <div class="col-12">
+                    <h6 class="text-start">Dine oplysninger</h6>
+                </div>
+            </div>
+        </div>
+        <div class="container pt-5 shadow-sm">
             <div class="row justify-content-center ">
                 <div class="col-12">
-                    <div class="row">
-                        <div class="col-6 col-md-3 col-lg-3">
+                    <div class="row mb-4">
+                        <label class="col-6 col-lg-3">
                             <input type="text" class="form-control col-6" id="inputfornavn"
                                    placeholder="Fornavn">
-                        </div>
-                        <div class="col-6 col-md-3 col-lg-3">
+                        </label>
+                        <label class="col-6 col-lg-3">
                             <input type="text" class="form-control col-6" id="inputefternavn"
                                    placeholder="Efternavn">
-                        </div>
+                        </label>
                     </div>
-                    <br>
-                    <div class="row">
-                        <div class="col-sm-12 col-md-6 col-lg-6">
+                    <div class="row mb-4">
+                        <label class="col-sm-12 col-lg-6">
                             <input type="text" class=" form-control col-md-auto col-lg-auto" id="inputadr"
                                    placeholder="Adresse">
-                        </div>
+                        </label>
                     </div>
-                    <br>
-                    <div class="row">
-                        <div class="col-6 col-md-3 col-lg-3">
+                    <div class="row mb-4">
+                        <label class="col-6 col-lg-3">
                             <input type="text" class="form-control col-6" id="inputfpostnr"
                                    placeholder="Postnr.">
-                        </div>
-                        <div class="col-6 col-md-3 col-lg-3">
+                        </label>
+                        <label class="col-6 col-lg-3">
                             <input type="text" class="form-control col-6" id="inputby"
                                    placeholder="By">
-                        </div>
+                        </label>
                     </div>
-                    <br>
-                    <div class="row">
-                        <div class="col-sm-12 col-md-6 col-lg-6">
+
+                    <div class="row mb-4">
+                        <label class="col-sm-12 col-lg-6">
                             <input type="email" class="form-control col-6" id="inputemail" placeholder="E-mail">
-                        </div>
+                        </label>
                     </div>
-                    <br>
-                    <div class="row">
-                        <div class="col-sm-12 col-md-6 col-lg-6">
+
+                    <div class="row mb-4">
+                        <label class="col-sm-12 col-lg-6">
                             <input type="email" class="form-control col-6" id="inputemail" placeholder="Gentag e-mail">
-                        </div>
+                        </label>
                     </div>
-                    <br>
-                    <div class="row">
-                        <div class="col-6 col-md-3 col-lg-3">
-                            <input type="text" class="form-control col-6" id="inputtelefon"
-                                   placeholder="Telefon">
-                        </div>
+
+                    <div class="row mb-4">
+                        <label class="col-6 col-lg-3"><input type="text" class="form-control col-6"
+                                                             id="inputtelefon"
+                                                             placeholder="Telefon">
+                        </label>
                     </div>
                 </div>
+            </div>
+        </div>
     </form>
 
-    <div class="container bg-light mt-5 py-2 rounded">
+
+    <div class="container col-12 d-none d-sm-none d-mg-block d-lg-block">
+        <div class="row gx-5 mt-5">
+            <div class="col">
+                <h6 class="p-2 col-auto text-start bg-light">Mulige afhentningstidspunkter</h6>
+            </div>
+            <div class="col">
+                <h6 class="p-2 text-start bg-light">Vælg betalingsmetode</h6>
+            </div>
+        </div>
+        <div class="row gx-5 align-items-center">
+            <div class="col">
+                <div class="p-3 shadow-sm">
+                    <h5 class="">10:30</h5>
+                    <h5 class="">12:30</h5>
+                    <h5 class="">14:30</h5>
+                    <h5 class="">16:30</h5>
+                </div>
+            </div>
+            <div class="col">
+                <div class="p-3 shadow-sm">
+                    <div class="d-none d-sm-none d-lg-block"><img
+                                src="https://i.imgur.com/OdxcctP.jpg"
+                                alt="" width="100"></div>
+                    <div class="d-none d-sm-none d-lg-block"><img
+                                src="https://i.imgur.com/5TqiRQV.jpg"
+                                alt="" width="100"></div>
+
+                    <div class="d-none d-sm-none d-lg-block"><img
+                                src="https://i.imgur.com/WIAP9Ku.jpg"
+                                alt="" width="100"></div>
+
+                    <div class="d-none d-sm-none d-lg-block"><img
+                                src="https://cdn.mobilepay.dk/res-website/img/icon/icon_mp_blue_monochrome.svg" alt=""
+                                width="40">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+    <div class="container bg-light mt-5 py-2 rounded d-mb-none d-lg-none">
         <div class="row">
             <div class="col-12">
                 <h6 class="mb-2 text-start">Mulige afhentnings tidspunkter</h6>
             </div>
         </div>
     </div>
-
-    <div class="container shadow-sm p-5">
+    <div class="container shadow-sm p-5 d-mb-none d-lg-none">
         <div class="row align-items-center">
-            <div class="col-3 col-md-1 t">10:30</div>
-            <div class="col-3 col-md-1 ">12:30</div>
-            <div class="col-3 col-md-1 ">14:30</div>
-            <div class="col-3 col-md-1 ">16:30</div>
+            <h5 class="col-3  ">10:30</h5>
+            <h5 class="col-3  ">12:30</h5>
+            <h5 class="col-3  ">14:30</h5>
+            <h5 class="col-3 ">16:30</h5>
         </div>
     </div>
-
-    <div class="col-3 col-md-1 d-none d-sm-none d-mb-block d-lg-block"><img src="https://i.imgur.com/OdxcctP.jpg"
-                                                                            alt=""></div>
-    <div class="col-3 col-md-1 d-none d-sm-none d-mb-block d-lg-block"><img src="https://i.imgur.com/5TqiRQV.jpg"
-                                                                            alt=""></div>
-    <div class="col-3 col-md-1 d-none d-sm-none d-mb-block d-lg-block"><img src="https://i.imgur.com/WIAP9Ku.jpg"
-                                                                            alt=""></div>
-    <div class="col-3 col-md-1 d-none d-sm-none d-mb-block d-lg-block"><img
-                src="https://cdn.mobilepay.dk/res-website/img/icon/icon_mp_blue_monochrome.svg" alt="" width="40"></div>
 
     <div class="container bg-light mt-5 py-2 rounded d-mb-none d-lg-none">
         <div class="row">
@@ -136,10 +179,10 @@
 
     <div class="container shadow-sm p-5 d-mb-none d-lg-none">
         <div class="row align-items-center">
-            <div class="col-3 col-md-1 text-center"><img src="https://i.imgur.com/OdxcctP.jpg" alt=""></div>
-            <div class="col-3 col-md-1 text-center"><img src="https://i.imgur.com/5TqiRQV.jpg" alt=""></div>
-            <div class="col-3 col-md-1 text-center"><img src="https://i.imgur.com/WIAP9Ku.jpg" alt=""></div>
-            <div class="col-3 col-md-1 text-center"><img
+            <div class="col-3 text-center"><img src="https://i.imgur.com/OdxcctP.jpg" alt=""></div>
+            <div class="col-3 text-center"><img src="https://i.imgur.com/5TqiRQV.jpg" alt=""></div>
+            <div class="col-3 text-center"><img src="https://i.imgur.com/WIAP9Ku.jpg" alt=""></div>
+            <div class="col-3 text-center"><img
                         src="https://cdn.mobilepay.dk/res-website/img/icon/icon_mp_blue_monochrome.svg" alt=""
                         width="40"></div>
         </div>
@@ -191,7 +234,7 @@
 <section class="">
     <div class="container mt-5 py-2 rounded">
         <div class="row">
-            <div class="col-12 col-sm-12 col-mb-6 col-lg-6">
+            <div class="col-mb-6 col-lg-6 col-xl-6 col-12 ">
                 <h6 class="mb-2 text-start bg-light p-3">Din kommentar</h6>
             </div>
         </div>
@@ -201,7 +244,7 @@
         <div class="row">
             <div class="col-12">
                 <label><textarea name="" cols="64" rows="3"
-                                 class="form-control bg-light">Yaaaash Queen</textarea></label>
+                                 class="form-control bg-light">Yaaaas Queen</textarea></label>
             </div>
         </div>
     </div>
@@ -217,22 +260,24 @@
 
     <div class="container mt-5 py-2 rounded col-12">
         <div class="row">
-            <div class="col-12">
-                <input type="checkbox" name="terms" id="terms" onchange="activateButton(this)"> Jeg bekræfter, at jeg har læst, forstået og accepteret Chocolate Clubs købsbetingelser.
-            </div>
+            <label class="col-12">
+                <input type="checkbox" name="terms" id="terms" onchange="activateButton(this)"> Jeg bekræfter, at jeg
+                har læst, forstået og accepteret Chocolate Clubs købsbetingelser.
+            </label>
         </div>
     </div>
 
-    <div class="container bg-light mt-5 py-2 rounded col-12">
+    <div class="container mt-5 py-2 rounded col-12 text-end">
         <div class="row">
             <div class="col-12">
-                <a href="betaling.php" class=" text-end btn btn-primary mb-4 btn-lg pl-5 pr-5">Bekræft
+                <a href="bestilling.php" class=" btn btn-primary mb-4 btn-lg pl-5 pr-5">Bekræft
                     betaling</a>
             </div>
         </div>
     </div>
-
 </section>
+
+
 
 </body>
 </html>
