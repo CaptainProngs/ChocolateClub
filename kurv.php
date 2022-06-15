017b01919005cddde168f11ed2a01cff697dbeeb
Siw <spersson@hotmail.com>
2022-06-15 09:47:27 +0200

    Rettet kurv

--- kurv.php
+++ kurv.php
@@ -19,95 +19,117 @@
 <body>
 
 <section class="pt-5 pb-5">
-    <div class="container text-wrap">
-        <div class="row w-100">
-            <div class="col-lg-12 col-md-12 col-12">
-                <h3 class="display-5 mb-2 text-center">Indkøbskurv</h3>
-                <p class="mb-5 text-center">
-                    <i class="text-info font-weight-bold">1</i> Vare i din kurv</p>
-                <div class="col-sm-6 mb-3 mb-m-1 order-md-1 text-md-left">
-                    <a href="webshop...">
-                        <i class=""><img src="images/Tegnebræt%208.png" alt="tilbage" width="30"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="table-responsive">
-        <table class="table">
-            <thead>
-            <tr>
-                <th scope="col" class="border-0 bg-light">
-                    <div class="p-2 px-3 text-uppercase">Product</div>
-                </th>
-                <th scope="col" class="border-0 bg-light">
-                    <div class="py-2 text-uppercase"></div>
-                </th>
-                <th scope="col" class="border-0 bg-light">
-                    <div class="py-2 text-uppercase">Antal</div>
-                </th>
-                <th scope="col" class="border-0 bg-light">
-                </th>
-                <th scope="col" class="border-0 bg-light">
-                    <div class="py-2 text-uppercase">Pris</div>
-                </th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <th scope="row" class="border-0">
-                    <div class="p-2">
-                        <img src="images/SoMeA.png" alt="" width="70" class="img-fluid rounded shadow-sm">
-                        <div class="ml-3 d-inline-block align-middle">
-                            <h5 class="mb-0"><a href="#" class="text-dark d-inline-block align-middle">Chokolade
-                                    fest</a></h5><span
-                                    class="text-muted font-weight-normal font-italic d-block">Lækkert chokolade</span>
-                        </div>
-                    </div>
-                </th>
-
-                <th scope="col" class="border-0">
-                    <div class="py-2 d-none"></div>
-                </th>
-                <td class="border-0 align-middle"><strong>1</strong></td>
-                <td class="border-0 align-middle"><img src="images/spand.png" alt="fjern"
-                                                       width="30 px">
-                <td class="border-0 align-middle"><strong>85 DKK</strong>
-                </td>
-            </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="bg-light rounded px-4 py-3 text-uppercase font-weight-bold">Kommentar til ordren</div>
-    <label><textarea name="" cols="30" rows="2" class="form-control "></textarea></label>
     <div class="container">
         <div class="row">
-            <div class="col-4 col-md-3 col-lg-2 py-2 bg-light rounded shadow-sm"> Click & Collect</div>
-            <div class="col-1"></div>
-            <div class="col-4 col-md-3 col-lg-2 py-2 bg-light rounded shadow-sm"> Levering</div>
-            <div class="col-3 col-md-6 col-lg-8"></div>
-            <div class="col-3 col-md-6 col-lg-8 "></div>
-        </div>
-    </div>
-    </div>
-    <div class="row">
-        <div class="float-right text-right">
-            <h4>Subtotal</h4>
-            <h1>85 DKK</h1>
-            <div class="row mt-4 d-flex justify-content-end">
-                <div class="col-sm-6 order-md-2 order-lg-2 text-right">
-                    <p>Gå til næste side for at vælge afhentningstidspunkt</p>
-                    <br>
-                    <a href="betaling.php" class="btn btn-primary mb-4 btn-lg pl-5 pr-5">Gå til
-                        betaling</a>
+            <div class="col-12">
+                <div class="row w-100">
+                    <h3 class="display-5 mb-2 text-center">Indkøbskurv</h3>
+                    <p class="mb-5 text-center">
+                        <i class="text-info font-weight-bold">1</i> Vare i din kurv</p>
+                    <div class="col-sm-6 mb-3 mb-m-1 order-md-1 text-md-left">
+                        <a href="webshop...">
+                            <i class=""><img src="images/Tegnebræt%208.png" alt="tilbage" width="30"></i></a>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <div class="container bg-light mt-5 py-2 rounded">
+            <div class="row">
+                <div class="col-6">
+                    <h4 class="mb-2 text-start">Produkt</h4>
+                </div>
+                <div class="col-3">
+                    <h6 class="mb-2 text-center">Antal</h6>
+                </div>
+                <div class="col-3">
+                    <h6 class="mb-2 text-center">Pris</h6>
+                </div>
+            </div>
+        </div>
+        <div class="container py-2 rounded shadow-sm">
+            <div class="row">
+                <div class="col-12">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <tbody>
+                            <tr>
+                                <th scope="row" class="border-0">
+                                    <div class="p-2">
+                                        <img src="images/SoMeA.png" alt="" width="70"
+                                             class="img-fluid rounded shadow-sm">
+                                        <div class="ml-3 d-inline-block align-middle">
+                                            <h5 class="mb-0"><a href="#" class="text-dark align-middle">Chokolade
+                                                    fest</a></h5><span
+                                                    class="text-muted font-weight-normal font-italic d-block">Lækkert chokolade</span>
+                                        </div>
+                                    </div>
+                                </th>
+                                <th scope="col" class="border-0">
+                                    <div class="py-2 d-none"></div>
+                                </th>
+                                <td class="border-0 align-middle"><strong>1</strong></td>
+                                <td class="border-0 align-middle"><img src="images/spand.png" alt="fjern"
+                                                                       width="30 px">
+                                <td class="border-0 align-middle"><strong>85 DKK</strong>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container mt-2">
+            <div class="row">
+                <div class="col-4 col-md-3 col-lg-2 py-2 bg-light rounded shadow-sm"> Click & Collect</div>
+                <div class="col-1"></div>
+                <div class="col-4 col-md-3 col-lg-2 py-2 bg-light rounded shadow-sm"> Levering</div>
+                <div class="col-3 col-md-6 col-lg-8"></div>
+                <div class="col-3 col-md-6 col-lg-8 "></div>
+            </div>
+        </div>
     </div>
-    </div>
-    </div>
+</section>
 
 
+<section class="">
+    <div class="container mt-5 py-2 rounded">
+        <div class="row">
+            <div class="col-mb-6 col-lg-6 col-xl-6 col-12 ">
+                <h5 class="mb-2 text-start bg-light p-3">Kommentar til ordren</h5>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <label><textarea name="" cols="64" rows="3"
+                                 class="form-control "></textarea></label>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-5 py-2 col-mb-12 col-lg-12">
+        <div class="row">
+            <div class="col-12">
+                <h4 class="text-end flex-semibold">Subtotal</h4>
+                <h1 class="text-end fw-bold">85 DKK</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-5 py-2 rounded col-12 text-end">
+        <div class="row">
+            <div class="col-12">
+                <h5>Gå til næste side for at vælge afhentningstidspunkt</h5>
+                <br>
+                <a href="bestilling.php" class=" btn btn-primary mb-4 btn-lg pl-5 pr-5">Gå til
+                    betaling</a>
+            </div>
+        </div>
+    </div>
 </section>
 
 
